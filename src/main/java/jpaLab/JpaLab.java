8ebd5e0a66186856adcbc4009c364b442e9080dc
unknown <a107316@MC0QW6RC.ww930.my-it-solutions.net>
2016-09-21 16:24:08 +0200

    elimina pruebas conexion

--- src/main/java/jpaLab/JpaLab.java
+++ src/main/java/jpaLab/JpaLab.java
@@ -1,26 +1,11 @@
 package jpaLab;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
 import javax.persistence.EntityManager;
 
 public class JpaLab {
 
 	public static void main(String[] args) throws ClassNotFoundException, Exception {
-		System.out.println("running");
-		Class.forName("com.mysql.jdbc.Driver");
-		// connect way #1
-		String url1 = "jdbc:mysql://localhost:3306/jpalab";
-		String user = "root";
-		String password = "!root";
 
-		Connection conn1 = DriverManager.getConnection(url1, user, password);
-		if (conn1 != null) {
-			System.out.println("Connected to the database test1");
-		}
-
-		// Persistence.createEntityManagerFactory("jpaLabPU");
 
 		Address address = new Address();
 		address.setLinea1("l1");
